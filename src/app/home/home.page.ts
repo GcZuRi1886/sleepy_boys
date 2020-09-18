8dfc992a5f316a8ec35677c84c0e41eec57d3d1c
tiekom <andrey.komlev@tie.ch>
2020-09-18 07:41:28 +0200

    - creat project structure - creat test view

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,17 @@ export class HomePage {
 
   constructor() {}
 
+  cardAction(value:string) : void {
+    let attributeName: string = document.getElementById(value).getAttribute('name');
+    if(attributeName==='chevron-down') {
+      <HTMLIonIconElement> <unknown> document.getElementById(value).setAttribute('name', 'chevron-up');
+    }else{
+      <HTMLIonIconElement> <unknown> document.getElementById(value).setAttribute('name', 'chevron-down');
+    }
+  }
+
+  checkIfOpen(value:string): boolean{
+    return document.getElementById(value).getAttribute('name')==='chevron-up';
+  }
+
 }
